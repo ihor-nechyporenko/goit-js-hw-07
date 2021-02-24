0e7e533eefb5f3c1f432dafb7a05f20c6b8e3554
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-02-24 13:13:18 +0200

    add task 5

---
+++ js/task-5.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+input.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    event.currentTarget.value.length === 0 
+        ? nameOutput.textContent = 'незнакомец' 
+        : nameOutput.textContent = event.currentTarget.value;
+}
