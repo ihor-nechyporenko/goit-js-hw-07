50b0d8c0feca3fa6f6007fd4582a7fd36c3ecd53
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-02-24 02:19:51 +0200

    add task 4

---
+++ js/task-4.js
@@ -0,0 +1,31 @@
+let counterValue = 0;
+
+const incrementBtn = document.querySelector('#counter').lastElementChild;
+const decrementBtn = document.querySelector('#counter').firstElementChild;
+
+function increment() {
+    counterValue += 1;
+    document.querySelector('#value').textContent = counterValue;
+}
+
+function decrement() {
+    counterValue -= 1;
+    document.querySelector('#value').textContent = counterValue;
+}
+
+incrementBtn.addEventListener('click', increment);
+
+decrementBtn.addEventListener('click', decrement);
+
+// VARIANT 2 ------------------------------------------------------------------
+
+// incrementBtn.addEventListener('click', () => {
+//     counterValue += 1;
+//     document.querySelector('#value').textContent = counterValue;
+// });
+
+// decrementBtn.addEventListener('click', () => {
+//     counterValue -= 1;
+//     document.querySelector('#value').textContent = counterValue;
+// })
+    
